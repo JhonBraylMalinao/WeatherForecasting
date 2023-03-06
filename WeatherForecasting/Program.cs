d2ff4c6b3c0098408831916301d7b1638151a990
Jhon Brayl Malinao <jhonbraylclear@gmail.com>
2023-03-07 07:53:45 +0800

    Update Program.cs

--- WeatherForecasting/Program.cs
+++ WeatherForecasting/Program.cs
@@ -63,31 +63,43 @@ public class WeatherForecasting
                         Console.WriteLine("\nBAYBAY CITY WEATHER INFORMATION");
                         Place Baybay = new Place("Baybay", "8", "10.6521° N", "124.8526° E");
                         Console.WriteLine(Baybay.ToString());
+                        Wind BaybayWind = new Wind(12, "East-West");
+                        Console.WriteLine(BaybayWind.ToString());
                         break;
                 case "2":
                         Console.WriteLine("\nHINDANG CITY WEATHER INFORMATION");
                         Place Hindang = new Place("Hindang", "8", "10.4305° N", "124.7442° E");
                         Console.WriteLine(Hindang.ToString());
+                        Wind HindangWind = new Wind(3, "North-East");
+                        Console.WriteLine(HindangWind.ToString());
                         break;
                 case "3":
                         Console.WriteLine("\nINOPACAN CITY WEATHER INFORMATION");
                         Place Inopacan = new Place("Inopacan", "8", "10.4995° N", "124.7414° E");
                         Console.WriteLine(Inopacan.ToString());
+                        Wind InopacanWind = new Wind(16, "South-West");
+                        Console.WriteLine(InopacanWind.ToString());
                         break;
                 case "4":
                         Console.WriteLine("\nMAASIN CITY WEATHER INFORMATION");
                         Place Maasin = new Place("Maasin", "8", "10.1654° N", "124.8403° E");
                         Console.WriteLine(Maasin.ToString());
+                        Wind MaasinWind = new Wind(24, "North-East");
+                        Console.WriteLine(MaasinWind.ToString());
                         break;
                 case "5":
                         Console.WriteLine("\nKANANGA CITY WEATHER INFORMATION");
                         Place Kananga = new Place("Kananga", "8", "11.1648° N", "124.5588° E");
                         Console.WriteLine(Kananga.ToString());
+                        Wind KanangaWind = new Wind(12, "East-West");
+                        Console.WriteLine(KanangaWind.ToString());
                         break;
                 case "6":
                         Console.WriteLine("\nTACLOBAN CITY WEATHER INFORMATION");
                         Place Tacloban = new Place("Tacloban", "8", "11.2543° N", "124.9617° E");
                         Console.WriteLine(Tacloban.ToString());
+                        Wind TaclobanWind = new Wind(12, "East-West");
+                        Console.WriteLine(TaclobanWind.ToString());
                         break;
                 default:
                     Console.WriteLine("Invalid Option. Try Again");
